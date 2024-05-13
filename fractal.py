c78ea5531529b6822c75b43079cfa017857ec4e5
Elliot Yun <elliot@elliots-mbp.lan>
2024-05-12 20:55:53 -0400

    created newton formula and convert integers to rgb

--- fractal.py
+++ fractal.py
@@ -1,10 +1,28 @@
-from PIL import Image
 import numpy as np
 import colorsys
+import sympy as sp
+from PIL import Image
 
-width = 1024
 
-# using polynomial z^3 - 1
-poly = np.poly1d([1, 0, 0, -1])
+# width of the image will be 1024
+WIDTH = 1024
 
-print(poly)
+# using polynomial x^3 - 1
+x = sp.symbols("x")
+f = x**3 - 1
+print("The polynomial formula we are using is: " + f)
+
+# converting integer to an rgb value as a tuple of colors
+
+
+def convert_rgb(i):
+    color = 255 * np.array(colorsys.hsv_to_rgb(i / 255.0, 1.0, 0.5))
+    return tuple(color.astype(int))
+
+
+# newton raphson formula
+
+
+def newton():
+    for _ in range(10):
+        guess = guess - (f / sp.Derivative(f, x))
